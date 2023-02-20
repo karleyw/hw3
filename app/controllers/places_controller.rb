8d3e5bbbff5f0e74e190c886abfc29e38417f8cf
karleyw <karley.woods@kellogg.northwestern.edu>
2023-02-20 18:02:27 +0000

    updates

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,11 +9,15 @@ def new
 end
 
 def show
+    @place = Place.find_by({ "id" => params["id"] })
+  end
 
-end
 
-
-def create
-end
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
+  end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
-
-    def new
+    
+      def new
         @post = Post.new
+      end
+    
+      def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
+        @post.save
+        redirect_to "/posts"
+      end
+    
     end
-
-def create
-end
-
-end
