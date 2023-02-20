5f8ee06b7afdcb0b489a4186ecec598236fa5d3c
karleyw <karley.woods@kellogg.northwestern.edu>
2023-02-20 05:51:01 +0000

    created resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources "list"
+  resources "mexico_city"
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
