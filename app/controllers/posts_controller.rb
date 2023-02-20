3d0aa981c465945cfb02911973c694f671dc4346
karleyw <karley.woods@kellogg.northwestern.edu>
2023-02-20 19:06:42 +0000

    updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @place["id"]
       end
     
       def create
@@ -11,7 +13,7 @@ class PostsController < ApplicationController
         @post["posted_on"] = params["post"]["posted_on"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
       end
     
     end
