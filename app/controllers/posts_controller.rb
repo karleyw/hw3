e26424e7f6b51b052b0afdc852f3357bfa43e441
karleyw <karley.woods@kellogg.northwestern.edu>
2023-02-20 19:34:10 +0000

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
       def new
         @post = Post.new
         @place = Place.find_by({ "id" => params["place_id"] })
-        @post["place_id"] = @place["id"]
       end
     
       def create
